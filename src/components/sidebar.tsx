bccc177a190d8d1d8f740a28ca7d4b401585efc1
Fady Gawargios <fadyg06@gmail.com>
2023-03-10 12:42:07 -0500

    Changed icons to match pages

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,27 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom"
-import { BsChatDotsFill, BsFillLightningFill, BsGearFill } from "react-icons/bs";
-import { FaFire, FaHome } from "react-icons/fa";
+import { BsChatLeftTextFill, BsFillLightningFill, BsGearFill, BsFillCalculatorFill } from "react-icons/bs";
+import { FaFire } from "react-icons/fa";
+import { TiHome } from "react-icons/ti";
+import { IoIosSettings } from "react-icons/io";
 // import { ObjectType } from "typescript";
 
 const SideBar: React.FC = () => {
   return (
     <div className="fixed top-0 left-0 flex flex-col w-16 h-screen mr-5 shadow-lg bg-primary text-secondary">
       <Link to="/"> 
-        <SideBarIcon icon={<FaHome size="28" />} text="Home" />
+        <SideBarIcon icon={<TiHome size="28" />} text="Home" />
       </Link>
       <Link to="/chat">
-        <SideBarIcon icon={<BsChatDotsFill size="28" />} text="chat" />
+        <SideBarIcon icon={<BsChatLeftTextFill size="23" />} text="chat" />
       </Link>
       <Link to="/calculator">
-        <SideBarIcon icon={<FaFire size="28" />} text="Calculator" />
+        <SideBarIcon icon={<BsFillCalculatorFill size="25" />} text="Calculator" />
       </Link>
       <Link to="/todo">
         <SideBarIcon icon={<BsFillLightningFill size="28" />} text="Todo" />
       </Link>
-      <div className="border-[2px] my-2 border-neutral-500 rounded-sm"></div>
+      <div className="border-[2px] my-1 border-neutral-500 rounded-sm"></div>
       <Link to="/settings">
-        <SideBarIcon icon={<BsGearFill size="28" />}  text="Settings"/>
+        <SideBarIcon icon={<IoIosSettings size="35" />}  text="Settings"/>
       </Link>
     </div>
   );
