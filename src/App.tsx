13e1d9ef2cd9c0a8d5634711cb292c5d6e60e361
Fady Gawargios <fadyg06@gmail.com>
2023-03-11 21:12:24 -0500

    Used state hook to update input

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import SideBar from "./components/sidebar"; 
 import ChannelBar from "./components/channelbar";
 import Home from "./components/pages/home";
+import Chat from "./components/pages/chat";
 // import Counter from "./components/functions/counter";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <ChannelBar/>
       <Routes>
         <Route path="/" element={<Home />}/>
+        <Route path="/chat" element={<Chat />}/>
       </Routes>
     </div>
   );
--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 // should take children components
 const Input: React.FC = () => {
+
+  let [field, updateField] = useState("HELLO");
+
   return (
-    <>
-    <input type="text">HI</input>
-    </>
+    <div className="text-black">
+      <input type="text" placeholder="Send msg" value={field} onChange={(e) => updateField(field = e.target.value)}></input>
+    </div>
   );
 };
+
+
 
 export default Input;
